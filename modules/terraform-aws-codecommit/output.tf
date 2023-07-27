841f6ef624888f4c3662ead7867e65c116d2a5db
Sharad Tiwari <sharadtiwari7860@gmail.com>
2023-07-27 14:40:26 +0530

    add aws codecommit module

---
+++ modules/terraform-aws-codecommit/output.tf
@@ -0,0 +1,13 @@
+output "repository_id" {
+    value = aws_codecommit_repository.codecommit_repository.repository_id
+}
+output "repository_arn" {
+    value = aws_codecommit_repository.codecommit_repository.arn
+}
+output "repository_http_url" {
+    value = aws_codecommit_repository.codecommit_repository.clone_url_http
+}
+
+output "repository_ssh_url" {
+    value = aws_codecommit_repository.codecommit_repository.clone_url_ssh
+}
