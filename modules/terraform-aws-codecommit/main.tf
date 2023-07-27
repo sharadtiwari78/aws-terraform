5ade3366a865af9ccff8bd6f43f61ee7228f470d
Sharad Tiwari <sharadtiwari7860@gmail.com>
2023-07-27 15:15:04 +0530

    add resource in codecommit

--- modules/terraform-aws-codecommit/main.tf
+++ modules/terraform-aws-codecommit/main.tf
@@ -1,7 +1,7 @@
 resource "aws_codecommit_repository" "codecommit_repository" {
   repository_name = var.repository_name
   description     = var.repo_description
-  default_branch = var.default_branch
+  default_branch  = var.default_branch
 
   tags = {
     Name = var.repository_name
