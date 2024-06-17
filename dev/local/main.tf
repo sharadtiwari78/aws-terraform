638172c276ca6b0c1736fcff61da2a72f097f284
Sharad Tiwari <110976728+sharadtiwari78@users.noreply.github.com>
2024-06-17 09:43:46 +0530

    Update main.tf
    Signed-off-by: Sharad Tiwari <110976728+sharadtiwari78@users.noreply.github.com>

--- dev/local/main.tf
+++ dev/local/main.tf
@@ -1,6 +1,6 @@
 module "codecommit" {
   source           = "../../modules/terraform-aws-codecommit"
-  repository_name  = demo-repo
-  repo_description = demo
-  default_branch   = main
+  repository_name  = "demo-repo"
+  repo_description = "demo"
+  default_branch   = "main"
 }
